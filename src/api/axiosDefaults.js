ed23f219f294bfdf3dae9913d30c517bc20a0fcd
Bartosz Stanczuk <synergy170185@gmail.com>
2024-10-02 13:52:21 +0000

    add authentications and sign up

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://drf-api-rec.herokuapp.com/";
+axios.defaults.baseURL = "https://rest-framework-project-d7776b473078.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
